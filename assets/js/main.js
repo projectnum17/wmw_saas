f0d69e87b0584dc71fc3c41b273e80f58ac80db0
projectnum17 <egor.chornyy@gmail.com>
2025-10-21 20:01:36 +0300

    fix names files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 'use strict';
 
-import headerHandler from './mods/_headerHandler.js';
-import mobileMenu from './mods/_mobileMenu.js';
-import videoHandler from './mods/_videoHandler.js';
-import scannerHandler from './mods/_scannerHandler.js';
-import historyHandler from './mods/_historyHandler.js';
-import sliders from './mods/_sliders.js';
-import statisticHandler from './mods/_statisticHandler.js';
-import faqHandler from './mods/_faqHandler.js';
+import headerHandler from './mods/headerHandler.js';
+import mobileMenu from './mods/mobileMenu.js';
+import videoHandler from './mods/videoHandler.js';
+import scannerHandler from './mods/scannerHandler.js';
+import historyHandler from './mods/historyHandler.js';
+import sliders from './mods/sliders.js';
+import statisticHandler from './mods/statisticHandler.js';
+import faqHandler from './mods/faqHandler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     headerHandler();
     mobileMenu();
     videoHandler();
-    // scannerHandler();
+    scannerHandler();
     sliders();
-    // historyHandler();
+    historyHandler();
     statisticHandler();
     faqHandler();
 });
